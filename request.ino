44fe4c107f2e14662e804634d334b58f09f8c172
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-04-27 07:41:59 +0800

    first commit

---
+++ request.ino
@@ -0,0 +1,200 @@
+#include <ESP8266WiFi.h>
+
+//Network Credentials
+const char* ssid     = "SSID";
+const char* password = "PASSWORD";
+
+int ledPinGreen = D5;
+int ledPinRed = D6;
+
+const char* host = ""; 
+
+int counter = 0;
+
+WiFiServer server(80);
+
+String header;
+
+String output5State = "off";
+String output4State = "off";
+
+const int output5 = 5;
+const int output4 = 4;
+
+
+unsigned long currentTime = millis();
+
+unsigned long previousTime = 0; 
+
+const long timeoutTime = 2000;
+
+void setup() {
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(output5, OUTPUT);
+  pinMode(output4, OUTPUT);
+  pinMode(ledPinGreen,OUTPUT);
+  pinMode(ledPinRed,OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output5, LOW);
+  digitalWrite(output4, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                            
+    Serial.println("New Client.");          
+    String currentLine = "";               
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { 
+      currentTime = millis();         
+      if (client.available()) {             
+        char c = client.read();            
+        Serial.write(c);                    
+        header += c;
+        if (c == '\n') {                    
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("GPIO 5 on");
+              output5State = "on";
+              digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("GPIO 5 off");
+              output5State = "off";
+              digitalWrite(output5, LOW);
+            } else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("GPIO 4 on");
+              output4State = "on";
+              digitalWrite(output4, HIGH);
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("GPIO 4 off");
+              output4State = "off";
+              digitalWrite(output4, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>HTTPS REQUESTER CPANEL</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+            client.println("<p>GPIO 5 - State " + output5State + "</p>");
+            // If the output5State is off, it displays the ON button       
+            if (output5State=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 4  
+            client.println("<p>GPIO 4 - State " + output4State + "</p>");
+            // If the output4State is off, it displays the ON button       
+            if (output4State=="off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+
+if (output5State == "on"){
+  counter++;
+  if (counter == 235000){
+    counter = 0;
+    WiFiClientSecure Client;
+      const int httpPort = 443; // 80 is for HTTP / 443 is for HTTPS!
+      
+      Client.setInsecure(); // this is the magical line that makes everything work
+      
+      if (!Client.connect(host, httpPort)) { //works!
+        Serial.println("connection failed");
+        digitalWrite(ledPinRed,HIGH);
+        delay(200);
+        digitalWrite(ledPinRed,LOW);
+        delay(200);
+        digitalWrite(ledPinRed,HIGH);
+        delay(200);
+        digitalWrite(ledPinRed,LOW);
+        return;
+      }
+    
+      else {
+    
+        // We now create a URI for the request
+        String url = "/arduino.php";
+        url += "?data=";
+        url += "aaaa";
+      
+      
+        Serial.print("Requesting URL: ");
+        Serial.println(url);
+      
+        // This will send the request to the server
+        Client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                     "Host: " + host + "\r\n" + 
+                     "Connection: close\r\n\r\n");
+      
+        Serial.println();
+        Serial.println("closing connection");
+         digitalWrite(ledPinGreen,HIGH);
+         delay(200);
+         digitalWrite(ledPinGreen,LOW);
+         delay(200);
+         digitalWrite(ledPinGreen,HIGH);
+         delay(200);
+         digitalWrite(ledPinGreen,LOW);
+    
+      }
+  }
+}
+}
